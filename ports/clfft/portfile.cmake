517f202a64e49fb8b12e1044c42f61b380a4b516
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2019-03-21 11:13:29 +0100

    Initial clFFT fix

--- ports/clfft/portfile.cmake
+++ ports/clfft/portfile.cmake
@@ -2,16 +2,24 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO clMathLibraries/clFFT
-    REF v2.12.2
+    REPO MathiasMagnus/clFFT
+    REF cache-fix
     SHA512 19e9a4e06f76ae7c7808d1188677d5553c43598886a75328b7801ab2ca68e35206839a58fe2f958a44a6f7c83284dc9461cd0e21c37d1042bf82e24aad066be8
-    HEAD_REF master
+    HEAD_REF cache-fix
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/tweak-install.patch
-)
+if(NOT VCPKG_CMAKE_SYSTEM_NAME) # Empty when Windows
+  vcpkg_apply_patches(
+      SOURCE_PATH ${SOURCE_PATH}
+      PATCHES ${CMAKE_CURRENT_LIST_DIR}/cmake.win.patch
+  )
+endif()
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  vcpkg_apply_patches(
+      SOURCE_PATH ${SOURCE_PATH}
+      PATCHES ${CMAKE_CURRENT_LIST_DIR}/cmake.linux.patch
+  )
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}/src
@@ -32,6 +40,11 @@ file(INSTALL
         ${CURRENT_PACKAGES_DIR}/share/clfft/copyright
 )
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "CMake")
+if(NOT VCPKG_CMAKE_SYSTEM_NAME) # Empty when Windows
+  vcpkg_fixup_cmake_targets(CONFIG_PATH "CMake")
+endif()
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/clFFT")
+endif()
 
 vcpkg_copy_pdbs()
